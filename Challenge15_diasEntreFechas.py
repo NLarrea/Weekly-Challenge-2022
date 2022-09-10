4bc3407fbac849eff2a03a55790a63fdbe53360d
NLarrea <larrea.naia@gmail.com>
2022-09-11 00:22:50 +0200

    Challenge15 añadido

---
+++ Challenge15_diasEntreFechas.py
@@ -0,0 +1,32 @@
+import datetime # import to use the date types and modules
+
+def formatChecker(dateStr:str):
+    i = 0
+    while(i<len(dateStr)):
+        if (i==2 or i==5) and dateStr[i]!='/': return False
+        if (i!=2 and i!=5) and (dateStr[i]<'0' or dateStr[i]>'9'): return False
+        i += 1
+    return True
+
+def insertData():
+    while True: # repeats in case of an error
+        try:
+            inputData = input("Write one date with the following format: dd/MM/yyyy\n")
+            ans = formatChecker(inputData) # checks the format
+            assert(ans) # raises an error if the format is not correct
+            inputData = inputData.split('/') # splits the string in three parts
+            date = datetime.datetime(int(inputData[2]), int(inputData[1]), int(inputData[0])) # creates a new datetime
+            return date # returns the date
+        except AssertionError: # if the format wasn't correct, raises this error
+            print("\nERROR: that is not the correct format!\n")
+        except ValueError: # if the value of the date is not correct (example: day = 35), raises this error
+            print("\nERROR: that is not a possible date!\n")
+
+date1 = insertData() # asks the first date
+date2 = insertData() # asks the second date
+#print("date1 - date2: {0}".format(abs(date1-date2)))
+date3 = abs(date1 - date2).days # calculates de difference in days
+date1 = date1.strftime("%d/%m/%Y") # changes the format to "date1" output
+date2 = date2.strftime("%d/%m/%Y") # changes the format to "date2" output
+# prints the result:
+print("Between \"{0}\" and \"{1}\" {2} {3} {4}".format(date1, date2, ("are" if date3>1 else "is"), date3, ("days" if date3>1 else "day")))
