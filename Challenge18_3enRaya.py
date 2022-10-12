5ed84d9fc45a4522dc5cdfba14bb9629feead1c4
NLarrea <larrea.naia@gmail.com>
2022-10-12 15:23:31 +0200

    Challenge18 modified, fixed bugs

--- Challenge18_3enRaya.py
+++ Challenge18_3enRaya.py
@@ -18,6 +18,8 @@
 # - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
 
 def checkTicTacToe(board: list):
+    xWins = False
+    oWins = False
     if len(board) != 3: return "Null" # the board has more than 3 rows
     # checks rows and columns
     for row in range(len(board)):
@@ -27,11 +29,11 @@ def checkTicTacToe(board: list):
         xCol = 0
         oCol = 0
         for col in range(len(board[row])):
-            # for the rows
+            # check rows
             if board[row][col] == "X": xRow += 1
             elif board[row][col] == "O": oRow += 1
             else: xRow,oRow = 0,0
-            # for columns
+            # check columns
             if board[col][row] == "X": xCol += 1
             elif board[col][row] == "O": oCol += 1
             else: xCol,oCol = 0,0
@@ -41,36 +43,40 @@ def checkTicTacToe(board: list):
             # who wins
             if xRow==3 or oRow==3 or xCol==3 or oCol==3:
                 if xRow==3 or xCol==3:
-                    return "X wins"
+                    xWins = True
                 elif oRow==3 or oCol==3:
-                    return "O wins"
-    # now we know the dimensions of the board are correct -> same rows and columns
-    # checks diagonals
-    xd1,xd2,od1,od2 = 0,0,0,0
-    for pos in range(len(board)):
-        if board[pos][pos] == "X": xd1 += 1
-        elif board[pos][pos] == "O": od1 += 1
-        if board[pos][3-pos-1] == "X": xd2 += 1
-        elif board[pos][3-pos-1] == "O": od2 += 1
-        # to see counters
-        # print("xd1: {0}\txd2: {1}".format(xd1,xd2))
-        # print("od1: {0}\tod2: {1}".format(od1,od2))
-        # who wins
-        if (xd1 or xd2) == 3: return "X wins"
-        elif (od1 or od2) == 3: return "O wins"
-    for row in range(len(board)):
-        for col in range(3):
-            if board[col][row] == "": return "Null"
-    return "Draw"
+                    oWins = True
+            # check if there is a winner
+            if (xWins or oWins) == True: break
+    # if there is not a winner yet -> check diagonals
+    if (xWins and oWins) != True:
+        # now we know the dimensions of the board are correct -> same rows and columns
+        # checks diagonals
+        xd1,xd2,od1,od2 = 0,0,0,0
+        for pos in range(len(board)):
+            if board[pos][pos] == "X": xd1 += 1
+            elif board[pos][pos] == "O": od1 += 1
+            if board[pos][3-pos-1] == "X": xd2 += 1
+            elif board[pos][3-pos-1] == "O": od2 += 1
+            # to see counters
+            # print("xd1: {0}\txd2: {1}".format(xd1,xd2))
+            # print("od1: {0}\tod2: {1}".format(od1,od2))
+            # who wins
+            if (xd1 or xd2) == 3: xWins = True
+            elif (od1 or od2) == 3: oWins = True
+    if (xWins and oWins) == True: return "Null, both are winners" # both wins
+    elif xWins == True: return "X wins"
+    elif oWins == True: return "O wins"
+    else: return "Draw" # there is not winner
 
 
 # create the arrays
 array1 = [["X","O","X"],["O","X","O"],["O","O","X"]] # X should win
-array2 = [["","O","X"],["","X","O"],["","O","X"]] # there are white spaces -> should be a null
+array2 = [["","O","X"],["","X","O"],["","O","X"]] # there is no winner -> Draw
 array3 = [["O","O","O"],["O","X","X"],["O","X","X"]] # O should win
 array4 = [["X","X","X"],["O","X","X"],["X","O","X"]] # X should win
 # see who is the winner
 print("Result 1: {0}".format(checkTicTacToe(array1))) # X wins
-print("Result 2: {0}".format(checkTicTacToe(array2))) # Null
+print("Result 2: {0}".format(checkTicTacToe(array2))) # Draw
 print("Result 3: {0}".format(checkTicTacToe(array3))) # O wins
 print("Result 4: {0}".format(checkTicTacToe(array4))) # X wins
